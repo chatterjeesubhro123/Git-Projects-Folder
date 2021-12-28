86914b55915b8c90b4b092b268dd9f830fe4484d
Subhronil Chatterjee <chatterjeesoumya16@gmail.com>
2021-12-29 02:14:52 +0530

    initial commit

---
+++ Sum.java
@@ -0,0 +1,11 @@
+class Sum{
+    public static void main(String[] args) {
+    int a=5,b=6;
+    System.out.println(a+b);
+}
+}
+
+// git init -> initializes a repository in the github
+// git status-.tells that what are the chnages tat are done in the current repository
+
+// to commit or add files we use -> git add <filename>
