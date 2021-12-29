ae0e82c51cd2456b23b38095a3c953a82b37525f
Subhronil Chatterjee <chatterjeesoumya16@gmail.com>
2021-12-29 19:16:29 +0530

    adding Sum.java after modifying

--- Sum.java
+++ Sum.java
@@ -1,7 +1,7 @@
 class Sum{
     public static void main(String[] args) {
     int a=5,b=6;
-    System.out.println(a+b);
+    System.out.println("The sum after changing is "+(a+b));
 }
 }
 
@@ -21,11 +21,19 @@ git add |->staging area--|
         //    staging area holds the changes before commiting to the git repository
 
         // suppose there are 40 files,we want to commit 14 of them,then at first we use git add for staging then git commit for commiting the files into the repository
-
+// git commit -m "initial message"
+// after -m "here we write any meaningful message to be shown"
         // [master (root-commit) 86914b5] initial commit
 //  1 file changed, 11 insertions(+)
 //  create mode 100644 Sum.java
 
 // git log -> to see how many commits are done where and how
+// clear -> clears git bash command screen
+
+// as currently newly created file is not added to the staging area,so on using git status it shows the newly created untracked files
+
+// fish shell-> to be installed for autocomplete features
+
+// when we use commit,every time new hex code generated and shown on the browser
 
 
