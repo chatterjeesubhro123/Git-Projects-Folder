a67fd15fb0b8c5ee8a76982fb31278f6a0ab0b2f
Subhronil Chatterjee <chatterjeesoumya16@gmail.com>
2021-12-29 19:12:51 +0530

    adding NewFile.java

--- Sum.java
+++ Sum.java
@@ -9,3 +9,23 @@ class Sum{
 // git status-.tells that what are the chnages tat are done in the current repository
 
 // to commit or add files we use -> git add <filename>
+// now while we write git status then it shows the file that is been added
+
+/*
+        |--working area
+        |
+git add |->staging area--|
+                         |git commit
+           repository <--- */
+
+        //    staging area holds the changes before commiting to the git repository
+
+        // suppose there are 40 files,we want to commit 14 of them,then at first we use git add for staging then git commit for commiting the files into the repository
+
+        // [master (root-commit) 86914b5] initial commit
+//  1 file changed, 11 insertions(+)
+//  create mode 100644 Sum.java
+
+// git log -> to see how many commits are done where and how
+
+
